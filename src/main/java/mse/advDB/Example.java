2295bfe9d1fd08ffc16604cae75a6ff3558d65c3
Nicolas Bedrunes <nicolas.bedrunes@hes-so.ch>
2022-12-12 19:42:20 +0100

    Exemple de requête qui fonctionne !

--- src/main/java/mse/advDB/Example.java
+++ src/main/java/mse/advDB/Example.java
@@ -13,31 +13,43 @@ public class Example {
     public static void main(String[] args) throws IOException, InterruptedException {
         String jsonPath = System.getenv("JSON_FILE");
         System.out.println("Path to JSON file is " + jsonPath);
-        int nbArticles = Integer.max(1000,Integer.parseInt(System.getenv("MAX_NODES")));
+        int nbArticles = Integer.max(1000, Integer.parseInt(System.getenv("MAX_NODES")));
         System.out.println("Number of articles to consider is " + nbArticles);
         String neo4jIP = System.getenv("NEO4J_IP");
         System.out.println("IP addresss of neo4j server is " + neo4jIP);
 
         Driver driver = GraphDatabase.driver("bolt://" + neo4jIP + ":7687", AuthTokens.basic("neo4j", "test"));
-	boolean connected = false;
-	do {
-	   try {
-	       System.out.println("Sleeping a bit waiting for the db");	   
-	       Thread.yield();   
-               Thread.sleep(5000); // let some time for the neo4j container to be up and running
+        boolean connected = false;
+        do {
+            try {
+                System.out.println("Sleeping a bit waiting for the db");
+                Thread.yield();
+                Thread.sleep(5000); // let some time for the neo4j container to be up and running
 
                 driver.verifyConnectivity();
-	        connected = true;
+                connected = true;
+            } catch (Exception e) {
             }
-	    catch(Exception e) {
-  	    }
-	} while(!connected);
+        } while (!connected);
         FileReader fr = new FileReader(jsonPath);
         BufferedReader br = new BufferedReader(fr);
         System.out.println("Reading first lines of the json file :");
-        for (int i = 0; i < 100 ; i++) {
+
+        // Les requêtes !!!
+        driver.session().run("MATCH (n) DETACH DELETE n");// delete all nodes
+        driver.session().run("CREATE (n:Node)");// example of query
+
+        // view on http://localhost:7474/browser/
+        // neo4j/test
+        // MATCH (n) RETURN n 
+
+        // base
+        for (int i = 0; i < 100; i++) {
             System.out.println(br.readLine());
         }
+        br.close();
+        fr.close();
+
         driver.close();
     }
 }
